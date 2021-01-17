manismku/listmonk

WIP: Add i18n support

frontend/src/views/Import.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="import">
-    <h1 class="title is-4">Import subscribers</h1>
-
+    <h1 class="title is-4">{{ $t('import.title') }}</h1>
     <b-loading :active="isLoading"></b-loading>
 
     <section v-if="isFree()" class="wrap-small">
@@ -12,23 +11,23 @@
               <b-field label="Mode">
                 <div>
                   <b-radio v-model="form.mode" name="mode"
-                    native-value="subscribe">Subscribe</b-radio>
+                    native-value="subscribe">{{ $t('import.subscriber') }}</b-radio>
                   <b-radio v-model="form.mode" name="mode"
-                    native-value="blocklist">Blocklist</b-radio>
+                    native-value="blocklist">{{ $t('import.blocklist') }}</b-radio>
                 </div>
               </b-field>
             </div>
             <div class="column">
               <b-field v-if="form.mode === 'subscribe'"
-                label="Overwrite?"
-                message="Overwrite name and attribs of existing subscribers?">
+                :label="$t('import.overwrite')"
+                :message="$t('import.overwriteHelp')">
                 <div>
                   <b-switch v-model="form.overwrite" name="overwrite" />
                 </div>
               </b-field>
             </div>
             <div class="column">
-              <b-field label="CSV delimiter" message="Default delimiter is comma."
+              <b-field :label="$t('import.csvDelim')" :message="$t('import.csvDelimHelp')"
                 class="delimiter">
                 <b-input v-model="form.delim" name="delim"
                   placeholder="," maxlength="1" required />
@@ -37,22 +36,22 @@
           </div>
 
           <list-selector v-if="form.mode === 'subscribe'"
-            label="Lists"
-            placeholder="Lists to subscribe to"
-            message="Lists to subscribe to."
+            :label="$t('globals.terms.lists')"
+            :placeholder="$t('import.listSubHelp')"
+            :message="$t('import.listSubHelp')"
             v-model="form.lists"
             :selected="form.lists"
             :all="lists.results"
           ></list-selector>
           <hr />
 
-          <b-field label="CSV or ZIP file" label-position="on-border">
+          <b-field :label="$t('import.csvFile')" label-position="on-border">
             <b-upload v-model="form.file" drag-drop expanded>
               <div class="has-text-centered section">
                 <p>
                   <b-icon icon="file-upload-outline" size="is-large"></b-icon>
                 </p>
-                <p>Click or drag a CSV or ZIP file here</p>
+                <p>{{ $t('import.csvFileHelp') }}</p>
               </div>
             </b-upload>
           </b-field>
@@ -64,20 +63,15 @@
           <div class="buttons">
             <b-button native-type="submit" type="is-primary"
               :disabled="!form.file || (form.mode === 'subscribe' && form.lists.length === 0)"
-              :loading="isProcessing">Upload</b-button>
+              :loading="isProcessing">{{ $t('import.upload') }}</b-button>
           </div>
         </div>
       </form>
       <br /><br />
 
       <div class="import-help">
-        <h5 class="title is-size-6">Instructions</h5>
-        <p>
-          Upload a CSV file or a ZIP file with a single CSV file in it to bulk
-          import subscribers. The CSV file should have the following headers
-          with the exact column names. <code>attributes</code> (optional)
-          should be a valid JSON string with double escaped quotes.
-        </p>
+        <h5 class="title is-size-6">{{ $t('import.instructions') }}</h5>
+        <p>{{ $t('import.instructionsHelp') }}</p>
         <br />
         <blockquote className="csv-example">
           <code className="csv-headers">
@@ -89,7 +83,7 @@
 
         <hr />
 
-        <h5 class="title is-size-6">Example raw CSV</h5>
+        <h5 class="title is-size-6">{{ $t('import.csvExample') }}</h5>
         <blockquote className="csv-example">
           <code className="csv-headers">
             <span>email,</span>
@@ -118,12 +112,14 @@
           {'has-text-danger': (status.status === 'failed' || status.status === 'stopped')}]">
         {{ status.status }}</p>
 
-      <p>{{ status.imported }} / {{ status.total }} records</p>
+      <p>{{ $t('import.recordsCount', { num: status.imported, total: status.total }) }}</p>
       <br />
 
       <p>
         <b-button @click="stopImport" :loading="isProcessing" icon-left="file-upload-outline"
-          type="is-primary">{{ isDone() ? 'Done' : 'Stop import' }}</b-button>
+          type="is-primary">
+          {{ isDone() ? $t('import.importDone') : $t('import.stopImport') }}
+        </b-button>
       </p>
       <br />
 
@@ -281,7 +277,7 @@ export default Vue.extend({
       this.$api.importSubscribers(params).then(() => {
         // On file upload, show a confirmation.
         this.$buefy.toast.open({
-          message: 'Import started',
+          message: this.$t('import.importStarted'),
           type: 'is-success',
           queue: false,
         });

frontend/src/views/Lists.vue
@@ -2,12 +2,15 @@
   <section class="lists">
     <header class="columns">
       <div class="column is-two-thirds">
-        <h1 class="title is-4">Lists
+        <h1 class="title is-4">
+          {{ $t('globals.terms.lists') }}
           <span v-if="!isNaN(lists.total)">({{ lists.total }})</span>
         </h1>
       </div>
       <div class="column has-text-right">
-        <b-button type="is-primary" icon-left="plus" @click="showNewForm">New</b-button>
+        <b-button type="is-primary" icon-left="plus" @click="showNewForm">
+          {{ $t('globals.buttons.new') }}
+        </b-button>
       </div>
     </header>
 
@@ -20,8 +23,9 @@
       backend-sorting @sort="onSort"
     >
         <template slot-scope="props">
-            <b-table-column field="name" label="Name" sortable width="25%"
-              paginated backend-pagination pagination-position="both" @page-change="onPageChange">
+            <b-table-column field="name" :label="$t('globals.fields.name')"
+              sortable width="25%" paginated backend-pagination pagination-position="both"
+              @page-change="onPageChange">
               <div>
                 <router-link :to="{name: 'subscribers_list', params: { listID: props.row.id }}">
                   {{ props.row.name }}
@@ -32,15 +36,17 @@
               </div>
             </b-table-column>
 
-            <b-table-column field="type" label="Type" sortable>
+            <b-table-column field="type" :label="$t('globals.fields.type')" sortable>
               <div>
-                <b-tag :class="props.row.type">{{ props.row.type }}</b-tag>
+                <b-tag :class="props.row.type">
+                  {{ $t('lists.types.' + props.row.type) }}
+                </b-tag>
                 {{ ' ' }}
                 <b-tag>
                   <b-icon :icon="props.row.optin === 'double' ?
                     'account-check-outline' : 'account-off-outline'" size="is-small" />
                   {{ ' ' }}
-                  {{ props.row.optin }}
+                  {{ $t('lists.optins.' + props.row.optin) }}
                 </b-tag>{{ ' ' }}
                 <a v-if="props.row.optin === 'double'" class="is-size-7 send-optin"
                   href="#" @click="$utils.confirm(null, () => createOptinCampaign(props.row))">
@@ -52,33 +58,34 @@
               </div>
             </b-table-column>
 
-            <b-table-column field="subscriber_count" label="Subscribers" numeric sortable centered>
+            <b-table-column field="subscriber_count" :label="$t('globals.terms.lists')"
+              numeric sortable centered>
                 <router-link :to="`/subscribers/lists/${props.row.id}`">
                   {{ props.row.subscriberCount }}
                 </router-link>
             </b-table-column>
 
-            <b-table-column field="created_at" label="Created" sortable>
+            <b-table-column field="created_at" :label="$t('globals.fields.createdAt')" sortable>
                 {{ $utils.niceDate(props.row.createdAt) }}
             </b-table-column>
-            <b-table-column field="updated_at" label="Updated" sortable>
+            <b-table-column field="updated_at" :label="$t('globals.fields.updatedAt')" sortable>
                 {{ $utils.niceDate(props.row.updatedAt) }}
             </b-table-column>
 
             <b-table-column class="actions" align="right">
               <div>
                 <router-link :to="`/campaigns/new?list_id=${props.row.id}`">
-                  <b-tooltip label="Send campaign" type="is-dark">
+                  <b-tooltip :label="$t('lists.sendCampaign')" type="is-dark">
                     <b-icon icon="rocket-launch-outline" size="is-small" />
                   </b-tooltip>
                 </router-link>
                 <a href="" @click.prevent="showEditForm(props.row)">
-                  <b-tooltip label="Edit" type="is-dark">
+                  <b-tooltip :label="$t('globals.buttons.edit')" type="is-dark">
                     <b-icon icon="pencil-outline" size="is-small" />
                   </b-tooltip>
                 </a>
                 <a href="" @click.prevent="deleteList(props.row)">
-                  <b-tooltip label="Delete" type="is-dark">
+                  <b-tooltip :label="$t('globals.buttons.delete')" type="is-dark">
                     <b-icon icon="trash-can-outline" size="is-small" />
                   </b-tooltip>
                 </a>
@@ -165,13 +172,13 @@ export default Vue.extend({
 
     deleteList(list) {
       this.$utils.confirm(
-        'Are you sure? This does not delete subscribers.',
+        this.$t('lists.confirmDelete'),
         () => {
           this.$api.deleteList(list.id).then(() => {
             this.getLists();
 
             this.$buefy.toast.open({
-              message: `'${list.name}' deleted`,
+              message: this.$t('globals.messages.deleted', { name: list.name }),
               type: 'is-success',
               queue: false,
             });
@@ -182,8 +189,8 @@ export default Vue.extend({
 
     createOptinCampaign(list) {
       const data = {
-        name: `Opt-in to ${list.name}`,
-        subject: `Confirm subscription(s) ${list.name}`,
+        name: this.$t('lists.optinTo', { name: list.name }),
+        subject: this.$t('lists.confirmSubscriptions', { name: list.name }),
         lists: [list.id],
         from_email: this.serverConfig.fromEmail,
         content_type: 'richtext',

muhammadmaqbool-dot/twosteps_emailer

frontend/src/views/Import.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="import">
-    <h1 class="title is-4">Import subscribers</h1>
-
+    <h1 class="title is-4">{{ $t('import.title') }}</h1>
     <b-loading :active="isLoading"></b-loading>
 
     <section v-if="isFree()" class="wrap-small">
@@ -12,23 +11,23 @@
               <b-field label="Mode">
                 <div>
                   <b-radio v-model="form.mode" name="mode"
-                    native-value="subscribe">Subscribe</b-radio>
+                    native-value="subscribe">{{ $t('import.subscribe') }}</b-radio>
                   <b-radio v-model="form.mode" name="mode"
-                    native-value="blocklist">Blocklist</b-radio>
+                    native-value="blocklist">{{ $t('import.blocklist') }}</b-radio>
                 </div>
               </b-field>
             </div>
             <div class="column">
               <b-field v-if="form.mode === 'subscribe'"
-                label="Overwrite?"
-                message="Overwrite name and attribs of existing subscribers?">
+                :label="$t('import.overwrite')"
+                :message="$t('import.overwriteHelp')">
                 <div>
                   <b-switch v-model="form.overwrite" name="overwrite" />
                 </div>
               </b-field>
             </div>
             <div class="column">
-              <b-field label="CSV delimiter" message="Default delimiter is comma."
+              <b-field :label="$t('import.csvDelim')" :message="$t('import.csvDelimHelp')"
                 class="delimiter">
                 <b-input v-model="form.delim" name="delim"
                   placeholder="," maxlength="1" required />
@@ -37,22 +36,22 @@
           </div>
 
           <list-selector v-if="form.mode === 'subscribe'"
-            label="Lists"
-            placeholder="Lists to subscribe to"
-            message="Lists to subscribe to."
+            :label="$t('globals.terms.lists')"
+            :placeholder="$t('import.listSubHelp')"
+            :message="$t('import.listSubHelp')"
             v-model="form.lists"
             :selected="form.lists"
             :all="lists.results"
           ></list-selector>
           <hr />
 
-          <b-field label="CSV or ZIP file" label-position="on-border">
+          <b-field :label="$t('import.csvFile')" label-position="on-border">
             <b-upload v-model="form.file" drag-drop expanded>
               <div class="has-text-centered section">
                 <p>
                   <b-icon icon="file-upload-outline" size="is-large"></b-icon>
                 </p>
-                <p>Click or drag a CSV or ZIP file here</p>
+                <p>{{ $t('import.csvFileHelp') }}</p>
               </div>
             </b-upload>
           </b-field>
@@ -64,20 +63,15 @@
           <div class="buttons">
             <b-button native-type="submit" type="is-primary"
               :disabled="!form.file || (form.mode === 'subscribe' && form.lists.length === 0)"
-              :loading="isProcessing">Upload</b-button>
+              :loading="isProcessing">{{ $t('import.upload') }}</b-button>
           </div>
         </div>
       </form>
       <br /><br />
 
       <div class="import-help">
-        <h5 class="title is-size-6">Instructions</h5>
-        <p>
-          Upload a CSV file or a ZIP file with a single CSV file in it to bulk
-          import subscribers. The CSV file should have the following headers
-          with the exact column names. <code>attributes</code> (optional)
-          should be a valid JSON string with double escaped quotes.
-        </p>
+        <h5 class="title is-size-6">{{ $t('import.instructions') }}</h5>
+        <p>{{ $t('import.instructionsHelp') }}</p>
         <br />
         <blockquote className="csv-example">
           <code className="csv-headers">
@@ -89,7 +83,7 @@
 
         <hr />
 
-        <h5 class="title is-size-6">Example raw CSV</h5>
+        <h5 class="title is-size-6">{{ $t('import.csvExample') }}</h5>
         <blockquote className="csv-example">
           <code className="csv-headers">
             <span>email,</span>
@@ -118,12 +112,14 @@
           {'has-text-danger': (status.status === 'failed' || status.status === 'stopped')}]">
         {{ status.status }}</p>
 
-      <p>{{ status.imported }} / {{ status.total }} records</p>
+      <p>{{ $t('import.recordsCount', { num: status.imported, total: status.total }) }}</p>
       <br />
 
       <p>
         <b-button @click="stopImport" :loading="isProcessing" icon-left="file-upload-outline"
-          type="is-primary">{{ isDone() ? 'Done' : 'Stop import' }}</b-button>
+          type="is-primary">
+          {{ isDone() ? $t('import.importDone') : $t('import.stopImport') }}
+        </b-button>
       </p>
       <br />
 
@@ -281,7 +277,7 @@ export default Vue.extend({
       this.$api.importSubscribers(params).then(() => {
         // On file upload, show a confirmation.
         this.$buefy.toast.open({
-          message: 'Import started',
+          message: this.$t('import.importStarted'),
           type: 'is-success',
           queue: false,
         });

frontend/src/views/Lists.vue
@@ -2,12 +2,15 @@
   <section class="lists">
     <header class="columns">
       <div class="column is-two-thirds">
-        <h1 class="title is-4">Lists
+        <h1 class="title is-4">
+          {{ $t('globals.terms.lists') }}
           <span v-if="!isNaN(lists.total)">({{ lists.total }})</span>
         </h1>
       </div>
       <div class="column has-text-right">
-        <b-button type="is-primary" icon-left="plus" @click="showNewForm">New</b-button>
+        <b-button type="is-primary" icon-left="plus" @click="showNewForm">
+          {{ $t('globals.buttons.new') }}
+        </b-button>
       </div>
     </header>
 
@@ -20,8 +23,9 @@
       backend-sorting @sort="onSort"
     >
         <template slot-scope="props">
-            <b-table-column field="name" label="Name" sortable width="25%"
-              paginated backend-pagination pagination-position="both" @page-change="onPageChange">
+            <b-table-column field="name" :label="$t('globals.fields.name')"
+              sortable width="25%" paginated backend-pagination pagination-position="both"
+              @page-change="onPageChange">
               <div>
                 <router-link :to="{name: 'subscribers_list', params: { listID: props.row.id }}">
                   {{ props.row.name }}
@@ -32,15 +36,17 @@
               </div>
             </b-table-column>
 
-            <b-table-column field="type" label="Type" sortable>
+            <b-table-column field="type" :label="$t('globals.fields.type')" sortable>
               <div>
-                <b-tag :class="props.row.type">{{ props.row.type }}</b-tag>
+                <b-tag :class="props.row.type">
+                  {{ $t('lists.types.' + props.row.type) }}
+                </b-tag>
                 {{ ' ' }}
                 <b-tag>
                   <b-icon :icon="props.row.optin === 'double' ?
                     'account-check-outline' : 'account-off-outline'" size="is-small" />
                   {{ ' ' }}
-                  {{ props.row.optin }}
+                  {{ $t('lists.optins.' + props.row.optin) }}
                 </b-tag>{{ ' ' }}
                 <a v-if="props.row.optin === 'double'" class="is-size-7 send-optin"
                   href="#" @click="$utils.confirm(null, () => createOptinCampaign(props.row))">
@@ -52,33 +58,34 @@
               </div>
             </b-table-column>
 
-            <b-table-column field="subscriber_count" label="Subscribers" numeric sortable centered>
+            <b-table-column field="subscriber_count" :label="$t('globals.terms.lists')"
+              numeric sortable centered>
                 <router-link :to="`/subscribers/lists/${props.row.id}`">
                   {{ props.row.subscriberCount }}
                 </router-link>
             </b-table-column>
 
-            <b-table-column field="created_at" label="Created" sortable>
+            <b-table-column field="created_at" :label="$t('globals.fields.createdAt')" sortable>
                 {{ $utils.niceDate(props.row.createdAt) }}
             </b-table-column>
-            <b-table-column field="updated_at" label="Updated" sortable>
+            <b-table-column field="updated_at" :label="$t('globals.fields.updatedAt')" sortable>
                 {{ $utils.niceDate(props.row.updatedAt) }}
             </b-table-column>
 
             <b-table-column class="actions" align="right">
               <div>
                 <router-link :to="`/campaigns/new?list_id=${props.row.id}`">
-                  <b-tooltip label="Send campaign" type="is-dark">
+                  <b-tooltip :label="$t('lists.sendCampaign')" type="is-dark">
                     <b-icon icon="rocket-launch-outline" size="is-small" />
                   </b-tooltip>
                 </router-link>
                 <a href="" @click.prevent="showEditForm(props.row)">
-                  <b-tooltip label="Edit" type="is-dark">
+                  <b-tooltip :label="$t('globals.buttons.edit')" type="is-dark">
                     <b-icon icon="pencil-outline" size="is-small" />
                   </b-tooltip>
                 </a>
                 <a href="" @click.prevent="deleteList(props.row)">
-                  <b-tooltip label="Delete" type="is-dark">
+                  <b-tooltip :label="$t('globals.buttons.delete')" type="is-dark">
                     <b-icon icon="trash-can-outline" size="is-small" />
                   </b-tooltip>
                 </a>
@@ -165,13 +172,13 @@ export default Vue.extend({
 
     deleteList(list) {
       this.$utils.confirm(
-        'Are you sure? This does not delete subscribers.',
+        this.$t('lists.confirmDelete'),
         () => {
           this.$api.deleteList(list.id).then(() => {
             this.getLists();
 
             this.$buefy.toast.open({
-              message: `'${list.name}' deleted`,
+              message: this.$t('globals.messages.deleted', { name: list.name }),
               type: 'is-success',
               queue: false,
             });
@@ -182,8 +189,8 @@ export default Vue.extend({
 
     createOptinCampaign(list) {
       const data = {
-        name: `Opt-in to ${list.name}`,
-        subject: `Confirm subscription(s) ${list.name}`,
+        name: this.$t('lists.optinTo', { name: list.name }),
+        subject: this.$t('lists.confirmSub', { name: list.name }),
         lists: [list.id],
         from_email: this.serverConfig.fromEmail,
         content_type: 'richtext',