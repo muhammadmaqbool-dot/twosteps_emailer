imfantuan/listmonk

Replace c3js with chart.js for major dep size reduction.

frontend/src/views/CampaignAnalytics.vue
@@ -49,33 +49,32 @@
     </p>
 
     <section class="charts mt-5">
-      <div class="chart columns" v-for="(v, k) in charts" :key="k">
-        <div class="column is-9">
-          <b-loading v-if="v.loading" :active="v.loading" :is-full-page="false" />
-          <h4 v-if="v.chart !== null">
-            {{ v.name }}
-            <span class="has-text-grey-light">({{ $utils.niceNumber(counts[k]) }})</span>
-          </h4>
-          <div :ref="`chart-${k}`" :id="`chart-${k}`" />
-        </div>
-        <div class="column is-2 donut-container">
-          <div :ref="`donut-${k}`" :id="`donut-${k}`" class="donut" />
+      <div class="chart" v-for="(v, k) in charts" :key="k">
+        <div class="columns">
+          <div class="column is-9">
+            <b-loading v-if="v.loading" :active="v.loading" :is-full-page="false" />
+            <h4 v-if="v.chart !== null">
+              {{ v.name }}
+              <span class="has-text-grey-light">({{ $utils.niceNumber(counts[k]) }})</span>
+            </h4>
+            <line-chart v-if="!v.loading" :data="v.data" />
+          </div>
+          <div class="column is-2 donut-container">
+            <donut-chart v-if="!v.loading" :data="v.donutData" />
+          </div>
         </div>
       </div>
     </section>
   </section>
 </template>
 
-<style lang="css">
-@import "c3/c3.css";
-</style>
-
 <script>
-import c3 from 'c3';
 import dayjs from 'dayjs';
 import Vue from 'vue';
 import { mapState } from 'vuex';
 import { colors } from '../constants';
+import LineChart from '../components/LineChart.vue';
+import DonutChart from '../components/DonutChart.vue';
 
 const chartColorRed = '#ee7d5b';
 const chartColors = [
@@ -90,6 +89,11 @@ const chartColors = [
 ];
 
 export default Vue.extend({
+  components: {
+    LineChart,
+    DonutChart,
+  },
+
   data() {
     return {
       isSearchLoading: false,
@@ -105,46 +109,37 @@ export default Vue.extend({
       charts: {
         views: {
           name: this.$t('campaigns.views'),
-          data: [],
+          data: null,
           fn: this.$api.getCampaignViewCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           loading: false,
         },
 
         clicks: {
           name: this.$t('campaigns.clicks'),
-          data: [],
+          data: null,
           fn: this.$api.getCampaignClickCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           loading: false,
         },
 
         bounces: {
           name: this.$t('globals.terms.bounces'),
-          data: [],
+          data: null,
           fn: this.$api.getCampaignBounceCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           donutColor: chartColorRed,
           loading: false,
         },
 
-        links: {
-          name: this.$t('analytics.links'),
-          data: [],
-          chart: null,
-          loading: false,
-          fn: this.$api.getCampaignLinkCounts,
-          chartFn: this.renderLinksChart,
-        },
+        // links: {
+        //   name: this.$t('analytics.links'),
+        //   data: null,
+        //   chart: null,
+        //   loading: false,
+        //   fn: this.$api.getCampaignLinkCounts,
+        //   chartFn: this.renderLinksChart,
+        // },
       },
 
       form: {
@@ -156,14 +151,6 @@ export default Vue.extend({
   },
 
   methods: {
-    formatDateTime(s) {
-      return dayjs(s).format('YYYY-MM-DD HH:mm');
-    },
-
-    isCampaignSelected(camp) {
-      return !this.form.campaigns.find(({ id }) => id === camp.id);
-    },
-
     onFromDateChange() {
       if (this.form.from > this.form.to) {
         this.form.to = dayjs(this.form.from).add(7, 'day').toDate();
@@ -176,95 +163,12 @@ export default Vue.extend({
       }
     },
 
-    renderLineChart(typ, data, el) {
-      const conf = {
-        bindto: el,
-        unload: true,
-        data: {
-          type: 'spline',
-          xs: {},
-          columns: [],
-          names: [],
-          colors: {},
-          empty: { label: { text: this.$t('globals.messages.emptyState') } },
-        },
-        axis: {
-          x: {
-            type: 'timeseries',
-            tick: {
-              format: '%Y-%m-%d %H:%M',
-            },
-          },
-        },
-        legend: {
-          show: false,
-        },
-      };
-
-      // Add campaign data to the chart.
-      data.forEach((c, n) => {
-        if (c.data.length === 0) {
-          return;
-        }
-
-        const x = `x${n + 1}`;
-        const d = `data${n + 1}`;
-
-        // data1, data2, datan => x1, x2, xn.
-        conf.data.xs[d] = x;
-
-        // Campaign name for each datan.
-        conf.data.names[d] = c.name;
-
-        // Dates for each xn.
-        conf.data.columns.push([x, ...c.data.map((v) => dayjs(v.timestamp))]);
-
-        // Counts for each datan.
-        conf.data.columns.push([d, ...c.data.map((v) => v.count)]);
-
-        // Colours for each datan.
-        conf.data.colors[d] = chartColors[n % data.length];
-      });
-
-      this.$nextTick(() => {
-        if (this.charts[typ].chart) {
-          this.charts[typ].chart.destroy();
-        }
-
-        this.charts[typ].chart = c3.generate(conf);
-      });
+    formatDateTime(s) {
+      return dayjs(s).format('YYYY-MM-DD HH:mm');
     },
 
-    renderDonutChart(typ, camps, data) {
-      const conf = {
-        bindto: this.$refs[`donut-${typ}`][0],
-        unload: true,
-        data: {
-          type: 'gauge',
-          columns: [],
-        },
-        gauge: {
-          width: 15,
-          max: 100,
-        },
-        color: {
-          pattern: [],
-        },
-      };
-
-      conf.gauge.max = camps.reduce((sum, c) => sum + c.sent, 0);
-      conf.data.columns.push([this.charts[typ].name, data.reduce((sum, d) => sum + d.count, 0)]);
-      conf.color.pattern.push(this.charts[typ].donutColor ?? chartColors[0]);
-
-      this.$nextTick(() => {
-        if (this.charts[typ].donut) {
-          this.charts[typ].donut.destroy();
-        }
-
-        if (conf.gauge.max > 0) {
-          this.charts[typ].donut = c3.generate(conf);
-        }
-      });
+    isCampaignSelected(camp) {
+      return !this.form.campaigns.find(({ id }) => id === camp.id);
     },
 
     renderLinksChart(typ, camps, data) {
@@ -316,48 +220,60 @@ export default Vue.extend({
         if (this.charts[typ].chart) {
           this.charts[typ].chart.destroy();
         }
-        this.charts[typ].chart = c3.generate(conf);
+        // this.charts[typ].chart = c3.generate(conf);
       });
     },
 
-    processLines(typ, camps, data) {
+    makeCharts(typ, campaigns, data) {
       // Make a campaign id => camp lookup map to group incoming
       // data by campaigns.
-      const campIDs = camps.reduce((obj, c) => {
+      const camps = campaigns.reduce((obj, c) => {
         const out = { ...obj };
         out[c.id] = c;
         return out;
       }, {});
 
-      // Group individual data points per campaign id.
-      // {1: [...], 2: [...]}
-      const groups = data.reduce((obj, d) => {
-        const out = { ...obj };
-        if (!(d.campaignId in out)) {
-          out[d.campaignId] = [];
-        }
+      const campIDs = Object.keys(camps);
 
-        out[d.campaignId].push(d);
-        return out;
-      }, {});
+      // datasets[] array for line chart.
+      const lines = campIDs.map((id, n) => {
+        const cId = parseInt(id, 10);
+        const points = data.filter((item) => item.campaignId === cId);
 
-      Object.keys(groups).forEach((k) => {
-        this.charts[typ].data.push({
-          name: campIDs[groups[k][0].campaignId].name,
-          data: groups[k],
-        });
+        return {
+          label: camps[id].name,
+          data: points.map((item) => ({ x: this.formatDateTime(item.timestamp), y: item.count })),
+          borderColor: chartColors[n % campIDs.length],
+          borderWidth: 2,
+          pointHoverBorderWidth: 5,
+          pointBorderWidth: 0.5,
+        };
       });
 
-      this.$nextTick(() => {
-        this.renderLineChart(typ, this.charts[typ].data, this.$refs[`chart-${typ}`][0]);
+      // Donut.
+      const labels = [];
+      const points = campIDs.map((id) => {
+        labels.push(camps[id].name);
+        const cId = parseInt(id, 10);
+        const sum = data.reduce((a, item) => (item.campaignId === cId ? a + item.count : a), 0);
+        return sum;
       });
+
+      const donut = {
+        labels,
+        datasets: [{
+          data: points, backgroundColor: chartColors, borderWidth: 6,
+        }],
+      };
+
+      return { lines: { datasets: lines }, donut };
     },
 
     onSubmit() {
       // Fetch count for each analytics type (views, counts, bounces);
       Object.keys(this.charts).forEach((k) => {
-        // Clear existing data.
-        this.charts[k].data = [];
+        this.charts[k].data = null;
+        this.charts[k].donutData = null;
 
         // Fetch views, clicks, bounces for every campaign.
         this.getData(k, this.form.campaigns);
@@ -393,10 +309,12 @@ export default Vue.extend({
         // Set the total count.
         this.counts[typ] = data.reduce((sum, d) => sum + d.count, 0);
 
-        this.charts[typ].chartFn(typ, camps, data);
+        const { lines, donut } = this.charts[typ].chartFn(typ, camps, data);
+        this.charts[typ].data = lines;
+        this.charts[typ].donutData = donut;
 
         if (this.charts[typ].donutFn && this.settings['privacy.individual_tracking']) {
-          this.charts[typ].donutFn(typ, camps, data);
+          this.charts[typ].donutData = this.charts[typ].donutFn(typ, camps, data);
         }
         this.charts[typ].loading = false;
       });

muhammadmaqbool-dot/twosteps_emailer

frontend/src/views/CampaignAnalytics.vue
@@ -49,33 +49,31 @@
     </p>
 
     <section class="charts mt-5">
-      <div class="chart columns" v-for="(v, k) in charts" :key="k">
-        <div class="column is-9">
-          <b-loading v-if="v.loading" :active="v.loading" :is-full-page="false" />
-          <h4 v-if="v.chart !== null">
-            {{ v.name }}
-            <span class="has-text-grey-light">({{ $utils.niceNumber(counts[k]) }})</span>
-          </h4>
-          <div :ref="`chart-${k}`" :id="`chart-${k}`" />
-        </div>
-        <div class="column is-2 donut-container">
-          <div :ref="`donut-${k}`" :id="`donut-${k}`" class="donut" />
+      <div class="chart" v-for="(v, k) in charts" :key="k">
+        <div class="columns">
+          <div class="column is-9">
+            <b-loading v-if="v.loading" :active="v.loading" :is-full-page="false" />
+            <h4 v-if="v.chart !== null">
+              {{ v.name }}
+              <span class="has-text-grey-light">({{ $utils.niceNumber(counts[k]) }})</span>
+            </h4>
+            <chart :type="v.type" v-if="!v.loading" :data="v.data" :on-click="v.onClick" />
+          </div>
+          <div class="column is-2 donut-container">
+            <chart type="donut" v-if="!v.loading" :data="v.donutData" />
+          </div>
         </div>
       </div>
     </section>
   </section>
 </template>
 
-<style lang="css">
-@import "c3/c3.css";
-</style>
-
 <script>
-import c3 from 'c3';
 import dayjs from 'dayjs';
 import Vue from 'vue';
 import { mapState } from 'vuex';
 import { colors } from '../constants';
+import Chart from '../components/Chart.vue';
 
 const chartColorRed = '#ee7d5b';
 const chartColors = [
@@ -90,6 +88,10 @@ const chartColors = [
 ];
 
 export default Vue.extend({
+  components: {
+    Chart,
+  },
+
   data() {
     return {
       isSearchLoading: false,
@@ -102,48 +104,45 @@ export default Vue.extend({
         bounces: 0,
         links: 0,
       },
+      urls: [],
       charts: {
         views: {
           name: this.$t('campaigns.views'),
-          data: [],
+          type: 'line',
+          data: null,
           fn: this.$api.getCampaignViewCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           loading: false,
         },
 
         clicks: {
           name: this.$t('campaigns.clicks'),
-          data: [],
+          type: 'line',
+          data: null,
           fn: this.$api.getCampaignClickCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           loading: false,
         },
 
         bounces: {
           name: this.$t('globals.terms.bounces'),
-          data: [],
+          type: 'line',
+          data: null,
           fn: this.$api.getCampaignBounceCounts,
-          chart: null,
-          chartFn: this.processLines,
-          donut: null,
-          donutFn: this.renderDonutChart,
+          chartFn: this.makeCharts,
           donutColor: chartColorRed,
           loading: false,
         },
 
         links: {
           name: this.$t('analytics.links'),
-          data: [],
+          type: 'bar',
+          data: null,
           chart: null,
           loading: false,
           fn: this.$api.getCampaignLinkCounts,
-          chartFn: this.renderLinksChart,
+          chartFn: this.makeLinksChart,
+          onClick: this.onLinkClick,
         },
       },
 
@@ -156,14 +155,6 @@ export default Vue.extend({
   },
 
   methods: {
-    formatDateTime(s) {
-      return dayjs(s).format('YYYY-MM-DD HH:mm');
-    },
-
-    isCampaignSelected(camp) {
-      return !this.form.campaigns.find(({ id }) => id === camp.id);
-    },
-
     onFromDateChange() {
       if (this.form.from > this.form.to) {
         this.form.to = dayjs(this.form.from).add(7, 'day').toDate();
@@ -176,131 +167,18 @@ export default Vue.extend({
       }
     },
 
-    renderLineChart(typ, data, el) {
-      const conf = {
-        bindto: el,
-        unload: true,
-        data: {
-          type: 'spline',
-          xs: {},
-          columns: [],
-          names: [],
-          colors: {},
-          empty: { label: { text: this.$t('globals.messages.emptyState') } },
-        },
-        axis: {
-          x: {
-            type: 'timeseries',
-            tick: {
-              format: '%Y-%m-%d %H:%M',
-            },
-          },
-        },
-        legend: {
-          show: false,
-        },
-      };
-
-      // Add campaign data to the chart.
-      data.forEach((c, n) => {
-        if (c.data.length === 0) {
-          return;
-        }
-
-        const x = `x${n + 1}`;
-        const d = `data${n + 1}`;
-
-        // data1, data2, datan => x1, x2, xn.
-        conf.data.xs[d] = x;
-
-        // Campaign name for each datan.
-        conf.data.names[d] = c.name;
-
-        // Dates for each xn.
-        conf.data.columns.push([x, ...c.data.map((v) => dayjs(v.timestamp))]);
-
-        // Counts for each datan.
-        conf.data.columns.push([d, ...c.data.map((v) => v.count)]);
-
-        // Colours for each datan.
-        conf.data.colors[d] = chartColors[n % data.length];
-      });
-
-      this.$nextTick(() => {
-        if (this.charts[typ].chart) {
-          this.charts[typ].chart.destroy();
-        }
-
-        this.charts[typ].chart = c3.generate(conf);
-      });
+    formatDateTime(s) {
+      return dayjs(s).format('YYYY-MM-DD HH:mm');
     },
 
-    renderDonutChart(typ, camps, data) {
-      const conf = {
-        bindto: this.$refs[`donut-${typ}`][0],
-        unload: true,
-        data: {
-          type: 'gauge',
-          columns: [],
-        },
-        gauge: {
-          width: 15,
-          max: 100,
-        },
-        color: {
-          pattern: [],
-        },
-      };
-
-      conf.gauge.max = camps.reduce((sum, c) => sum + c.sent, 0);
-      conf.data.columns.push([this.charts[typ].name, data.reduce((sum, d) => sum + d.count, 0)]);
-      conf.color.pattern.push(this.charts[typ].donutColor ?? chartColors[0]);
-
-      this.$nextTick(() => {
-        if (this.charts[typ].donut) {
-          this.charts[typ].donut.destroy();
-        }
-
-        if (conf.gauge.max > 0) {
-          this.charts[typ].donut = c3.generate(conf);
-        }
-      });
+    isCampaignSelected(camp) {
+      return !this.form.campaigns.find(({ id }) => id === camp.id);
     },
 
-    renderLinksChart(typ, camps, data) {
-      const conf = {
-        bindto: this.$refs[`chart-${typ}`][0],
-        unload: true,
-        data: {
-          type: 'bar',
-          x: 'x',
-          columns: [],
-          color: (c, d) => (typeof (d) === 'object' ? chartColors[d.index % data.length] : chartColors[0]),
-          empty: { label: { text: this.$t('globals.messages.emptyState') } },
-          onclick: (d) => {
-            window.open(data[d.index].url, '_blank', 'noopener noreferrer');
-          },
-        },
-        bar: {
-          width: {
-            max: 30,
-          },
-        },
-        axis: {
-          rotated: true,
-          x: {
-            type: 'category',
-            tick: {
-              multiline: false,
-            },
-          },
-        },
-      };
-
-      // Add link data to the chart.
-      // https://c3js.org/samples/axes_x_tick_rotate.html
-      conf.data.columns.push(['x', ...data.map((l) => {
+    makeLinksChart(typ, camps, data) {
+      const labels = data.map((l) => {
         try {
+          this.urls.push(l.url);
           const u = new URL(l.url);
           if (l.url.length > 80) {
             return `${u.hostname}${u.pathname.substr(0, 50)}..`;
@@ -309,59 +187,67 @@ export default Vue.extend({
         } catch {
           return l.url;
         }
-      })]);
-      conf.data.columns.push([this.$t('analytics.count'), ...data.map((l) => l.count)]);
-
-      this.$nextTick(() => {
-        if (this.charts[typ].chart) {
-          this.charts[typ].chart.destroy();
-        }
-        this.charts[typ].chart = c3.generate(conf);
       });
+
+      const out = {
+        labels,
+        datasets: [
+          {
+            data: data.map((l) => l.count),
+            backgroundColor: chartColors,
+          }],
+      };
+
+      return { points: out, donut: null };
     },
 
-    processLines(typ, camps, data) {
+    makeCharts(typ, campaigns, data) {
       // Make a campaign id => camp lookup map to group incoming
       // data by campaigns.
-      const campIDs = camps.reduce((obj, c) => {
+      const camps = campaigns.reduce((obj, c) => {
         const out = { ...obj };
         out[c.id] = c;
         return out;
       }, {});
 
-      // Group individual data points per campaign id.
-      // {1: [...], 2: [...]}
-      const groups = data.reduce((obj, d) => {
-        const out = { ...obj };
-        if (!(d.campaignId in out)) {
-          out[d.campaignId] = [];
-        }
+      const campIDs = Object.keys(camps);
 
-        out[d.campaignId].push(d);
-        return out;
-      }, {});
+      // datasets[] array for line chart.
+      const lines = campIDs.map((id, n) => {
+        const cId = parseInt(id, 10);
+        const points = data.filter((item) => item.campaignId === cId);
 
-      Object.keys(groups).forEach((k) => {
-        this.charts[typ].data.push({
-          name: campIDs[groups[k][0].campaignId].name,
-          data: groups[k],
-        });
+        return {
+          label: camps[id].name,
+          data: points.map((item) => ({ x: this.formatDateTime(item.timestamp), y: item.count })),
+          borderColor: chartColors[n % campIDs.length],
+          borderWidth: 2,
+          pointHoverBorderWidth: 5,
+          pointBorderWidth: 0.5,
+        };
       });
 
-      this.$nextTick(() => {
-        this.renderLineChart(typ, this.charts[typ].data, this.$refs[`chart-${typ}`][0]);
+      // Donut.
+      const labels = [];
+      const points = campIDs.map((id) => {
+        labels.push(camps[id].name);
+        const cId = parseInt(id, 10);
+        const sum = data.reduce((a, item) => (item.campaignId === cId ? a + item.count : a), 0);
+        return sum;
       });
+
+      const donut = {
+        labels,
+        datasets: [{
+          data: points, backgroundColor: chartColors, borderWidth: 6,
+        }],
+      };
+
+      return { points: { datasets: lines }, donut };
     },
 
     onSubmit() {
-      // Fetch count for each analytics type (views, counts, bounces);
-      Object.keys(this.charts).forEach((k) => {
-        // Clear existing data.
-        this.charts[k].data = [];
-
-        // Fetch views, clicks, bounces for every campaign.
-        this.getData(k, this.form.campaigns);
-      });
+      this.$router.push({ query: { id: this.form.campaigns.map((c) => c.id) } });
     },
 
     queryCampaigns(q) {
@@ -393,13 +279,18 @@ export default Vue.extend({
         // Set the total count.
         this.counts[typ] = data.reduce((sum, d) => sum + d.count, 0);
 
-        this.charts[typ].chartFn(typ, camps, data);
-
-        if (this.charts[typ].donutFn && this.settings['privacy.individual_tracking']) {
-          this.charts[typ].donutFn(typ, camps, data);
-        }
+        const { points, donut } = this.charts[typ].chartFn(typ, camps, data);
+        this.charts[typ].data = points;
+        this.charts[typ].donutData = donut;
         this.charts[typ].loading = false;
       });
+    },
+
+    onLinkClick(e) {
+      const bars = e.chart.getElementsAtEventForMode(e, 'nearest', { intersect: true }, true);
+      if (bars.length > 0) {
+        window.open(this.urls[bars[0].index], '_blank', 'noopener noreferrer');
+      }
     },
   },
 
@@ -432,7 +323,15 @@ export default Vue.extend({
 
         this.$nextTick(() => {
           this.isSearchLoading = false;
-          this.onSubmit();
+
+          // Fetch count for each analytics type (views, counts, bounces);
+          Object.keys(this.charts).forEach((k) => {
+            this.charts[k].data = null;
+            this.charts[k].donutData = null;
+
+            // Fetch views, clicks, bounces for every campaign.
+            this.getData(k, this.form.campaigns);
+          });
         });
       });
     }